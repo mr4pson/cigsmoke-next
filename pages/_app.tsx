cc0fa37a5cb873c7bbf0d1ac1d28c55214f4fa92
Evgenii Goriachkin <Evgenii.Goriachkin@payever.org>
2022-07-02 15:38:52 +0300

    Added success notification message

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
 import 'antd/dist/antd.css';
 import AdminLayout from 'components/admin-layout/layout';
 import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { Page, paths } from 'routes/constants';
 import 'styles.css';
 import { wrapper } from '../redux/store';
 
 function App({ Component, pageProps }: AppProps) {
-  return (
+  const router = useRouter();
+
+  return router.pathname !== paths[Page.LOGIN] ? (
     <AdminLayout>
       <Component {...pageProps} />
     </AdminLayout>
+  ) : (
+    <Component {...pageProps} />
   );
 }
 
