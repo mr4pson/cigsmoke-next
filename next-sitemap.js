08764f2560b026d1311eebc69faac8ed4e25c7e7
AhmadReshadarm <armreshad@gmail.com>
2022-07-31 18:28:14 +0300

    dynamic sitemap generator is added

---
+++ next-sitemap.js
@@ -0,0 +1,27 @@
+const siteUrl = 'https://www.cigsmok.ru';
+
+module.exports = {
+  siteUrl,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        disallow: '/admin',
+      },
+      {
+        userAgent: '*',
+        disallow: '/admin/*',
+      },
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    additionalSitemaps: [
+      `${siteUrl}/sitemap.xml`,
+      `${siteUrl}/server-sitemap.xml`,
+    ],
+  },
+  exclude: ['/admin', '/admin/*'],
+};
