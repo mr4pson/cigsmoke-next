dca80e8e7aea1ea6628f27120bbae71747f94b7e
Rishad <armreshad@gmail.com>
2022-08-22 17:34:29 +0300

    location component intials

---
+++ components/store/storeLayout/utils/LocationFinder/index.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import color from 'components/store/lib/ui.colors';
+import variants from 'components/store/lib/variants';
+
+const LocationFinder = () => {
+  return <></>;
+};
+
+const Container = styled.div`
+width:100%;
+height:100%;
+
+`
+
+
+export default LocationFinder;
